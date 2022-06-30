14bc2fba2e05eb9411dc9ca35634f3a83f44610f
vinicius rosa <viniciushrk@hotmail.com>
2022-06-29 20:41:23 -0400

    Adicionar arquivos de projeto.

---
+++ BlazorFullStackCrud/Server/Data/DataContext.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BlazorFullStackCrud.Server.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Comic>().HasData(
+                new Comic { Id = 1, Name = "Marvel" },
+                new Comic { Id = 2, Name = "DC" }
+            );
+            modelBuilder.Entity<SuperHero>().HasData(
+                new SuperHero
+                {
+                    Id = 4,
+                    FirstName = "Tony",
+                    LastName = "Stark",
+                    HeroName = "Homem de ferro",
+                    ComicId = 1
+                },
+                 new SuperHero
+                 {
+                     Id = 3,
+                     FirstName = "Coringa",
+                     LastName = "",
+                     HeroName = "Voringa",
+                     ComicId = 2
+                 },
+                new SuperHero
+                {
+                    Id = 1,
+                    FirstName = "Peter",
+                    LastName = "Parker",
+                    HeroName = "Spiderman",
+                    ComicId = 1
+                },
+                new SuperHero
+                {
+                    Id = 2,
+                    FirstName = "Bruce",
+                    LastName = "Wayne",
+                    HeroName = "Batman",
+                    ComicId = 2
+                }
+            );
+        }
+
+        public DbSet<SuperHero> SuperHeroes { get; set; }
+        public DbSet<Comic> Comics { get; set; }
+    }
+}
---
+++ BlazorFullStackCrud/Server/Migrations/20220630003533_InitialCreate.cs
@@ -0,0 +1,91 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BlazorFullStackCrud.Server.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Comics",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Comics", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "SuperHeroes",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    FirstName = table.Column<string>(type: "TEXT", nullable: false),
+                    LastName = table.Column<string>(type: "TEXT", nullable: false),
+                    HeroName = table.Column<string>(type: "TEXT", nullable: false),
+                    ComicId = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_SuperHeroes", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_SuperHeroes_Comics_ComicId",
+                        column: x => x.ComicId,
+                        principalTable: "Comics",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Comics",
+                columns: new[] { "Id", "Name" },
+                values: new object[] { 1, "Marvel" });
+
+            migrationBuilder.InsertData(
+                table: "Comics",
+                columns: new[] { "Id", "Name" },
+                values: new object[] { 2, "DC" });
+
+            migrationBuilder.InsertData(
+                table: "SuperHeroes",
+                columns: new[] { "Id", "ComicId", "FirstName", "HeroName", "LastName" },
+                values: new object[] { 1, 1, "Peter", "Spiderman", "Parker" });
+
+            migrationBuilder.InsertData(
+                table: "SuperHeroes",
+                columns: new[] { "Id", "ComicId", "FirstName", "HeroName", "LastName" },
+                values: new object[] { 2, 2, "Bruce", "Batman", "Wayne" });
+
+            migrationBuilder.InsertData(
+                table: "SuperHeroes",
+                columns: new[] { "Id", "ComicId", "FirstName", "HeroName", "LastName" },
+                values: new object[] { 3, 2, "Coringa", "Voringa", "" });
+
+            migrationBuilder.InsertData(
+                table: "SuperHeroes",
+                columns: new[] { "Id", "ComicId", "FirstName", "HeroName", "LastName" },
+                values: new object[] { 4, 1, "Tony", "Homem de ferro", "Stark" });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_SuperHeroes_ComicId",
+                table: "SuperHeroes",
+                column: "ComicId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "SuperHeroes");
+
+            migrationBuilder.DropTable(
+                name: "Comics");
+        }
+    }
+}
